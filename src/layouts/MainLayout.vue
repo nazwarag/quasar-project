fe194b4311e221fef27babf61816152429c462f9
nazwarag <143011995+nazwarag@users.noreply.github.com>
2023-11-09 11:36:21 +0700

    update

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,8 +1,8 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated color="positive">
+    <q-header elevated class="bg-positive">
       <q-toolbar>
-        <q-toolbar-title class="q-mx-auto" style="font-family: 'Poppins'; font-style: italic;">
+        <q-toolbar-title class="font-title">
           Berita Terkini
         </q-toolbar-title>
 
@@ -24,27 +24,51 @@
                 icon="search"
                 aria-label="Cari"
                 @click="searchNews"
-                />
-              </template>
-            </q-input>
-          </div>
-
+              />
+            </template>
+          </q-input>
+        </div>
       </q-toolbar>
+
+      <div class="q-pa-md q-gutter-lg">
+        <q-banner inline-actions rounded class="bg-white">
+            <a
+              v-for="category in categoryOptions"
+              :key="category.value"
+              dense
+              @click="selectCategory(category.value)"
+              :class="[
+                'q-mx-sm',
+                'cursor-pointer',
+                selectedCategory === category.value ? 'font-weight-bold text-black' : 'font-weight-regular text-positive'
+              ]"
+            >
+              {{ category.label }}
+            </a>
+          </q-banner>
+        </div>
     </q-header>
 
     <q-page-container>
       <q-card-section>
         <div class="q-pa-md row items-start q-gutter-md" style="justify-content: center;">
           <div
-            v-for="(article, index) in beritaList" :key="index" class="q-mb-md" style="width: 450px"
+            v-for="(article, index) in beritaList"
+            :key="index"
+            class="q-mb-md"
+            style="width: 450px"
           >
-            <q-card class="my-card" clickable @click="tampilkanDetailBerita(article)" style="cursor: pointer;">
+            <q-card
+              class="my-card"
+              clickable
+              @click="tampilkanDetailBerita(article)"
+              style="cursor: pointer;"
+            >
               <q-img :src="article.urlToImage" alt=" ">
                 <div class="absolute-bottom text-h6">
                   {{ article.title }}
                 </div>
               </q-img>
-
               <q-card-section style="font-family: 'Poppins'; font-size: 1.4rem;">
                 {{ article.description }}
               </q-card-section>
@@ -62,17 +86,24 @@ import axios from 'axios'
 
 export default defineComponent({
   name: 'MainLayout',
-
   setup () {
     const beritaList = ref([])
     const searchTerm = ref('')
-    const categories = ref(['business', 'health', 'sports', 'entertainment', 'science'])
+    const selectedCategory = ref('general')
+    const categoryOptions = ref([
+      { label: 'General', value: 'general' },
+      { label: 'Business', value: 'business' },
+      { label: 'Health', value: 'health' },
+      { label: 'Sports', value: 'sports' },
+      { label: 'Entertainment', value: 'entertainment' },
+      { label: 'Science', value: 'science' }
+    ])
 
     const tampilkanDetailBerita = (berita) => {
       window.open(berita.url)
     }
 
-    const fetchNews = async (category) => {
+    const fetchNews = async () => {
       try {
         const apiKey = '959823a4ed2f4f9095156ec4c82fd957'
         const apiUrl = 'https://newsapi.org/v2/top-headlines'
@@ -80,7 +111,7 @@ export default defineComponent({
         const response = await axios.get(apiUrl, {
           params: {
             country: 'us',
-            category: { category }
+            category: selectedCategory.value
           },
           headers: {
             'X-Api-Key': apiKey
@@ -119,17 +150,34 @@ export default defineComponent({
       }
     }
 
+    const selectCategory = async (category) => {
+      selectedCategory.value = category
+      await fetchNews()
+    }
+
     onMounted(() => {
-      fetchNews(categories)
+      fetchNews()
     })
 
     return {
       beritaList,
       tampilkanDetailBerita,
       searchTerm,
-      categories,
-      searchNews
+      selectedCategory,
+      categoryOptions,
+      searchNews,
+      selectCategory
     }
   }
 })
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap');
+  .font-title {
+    font-family: 'Poppins', sans-serif;
+    font-weight: 500;
+    font-size: 3rem;
+    margin-left: 20px;
+  }
+</style>
